e60bde9b6bbe8ad4b4e2d2b1006b5f69556af857
xiaowu5759 <kuaixiang789@qq.com>
2021-05-28 16:15:41 +0800

    添加配置类读取功能

---
+++ src/main/java/com/xiaowu5759/common/config/JDBCProperties.java
@@ -0,0 +1,60 @@
+package com.xiaowu5759.common.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.stereotype.Component;
+
+/**
+ * @author xiaowu
+ * @date 2021/5/28 4:09 PM
+ */
+@PropertySource(value = "classpath:prop/jdbc.properties")
+@Component
+public class JDBCProperties {
+
+    @Value("${url}")
+    private String url;
+
+    @Value("${username}")
+    private String username;
+
+    @Value("${password}")
+    private String password;
+
+    @Value("${driver}")
+    private String driver;
+
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getDriver() {
+        return driver;
+    }
+
+    public void setDriver(String driver) {
+        this.driver = driver;
+    }
+}
+
--- src/main/java/com/xiaowu5759/common/manager/RedisClient.java
+++ src/main/java/com/xiaowu5759/common/manager/RedisClient.java
@@ -1,5 +1,7 @@
 package com.xiaowu5759.common.manager;
 
+import org.springframework.context.annotation.Configuration;
+
 /**
  * 最好是 连接池化
  * bean 单例化
@@ -7,5 +9,6 @@ package com.xiaowu5759.common.manager;
  * @author xiaowu
  * @date 2021/5/14 2:28 PM
  */
+@Configuration
 public class RedisClient {
 }
