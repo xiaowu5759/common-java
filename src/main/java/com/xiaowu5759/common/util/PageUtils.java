f371ec26adab391b291376e2533462778b2554b2
xiaowu5759 <kuaixiang789@qq.com>
2021-08-08 13:40:41 +0800

    添加最新pom包

---
+++ src/main/java/com/xiaowu5759/common/util/PageUtils.java
@@ -0,0 +1,11 @@
+package com.xiaowu5759.common.util;
+
+/**
+ * 在翻页场景中，用户输入参数的小于1，则前端返回第一页参数给后端；
+ * 后端发现用户输入的参数大于总页数，直接返回最后一页。
+ *
+ * @author xiaowu5759
+ * @since 2021/8/8 13:30
+ */
+public class PageUtils {
+}
---
+++ src/main/java/com/xiaowu5759/common/web/WebConfig.java
@@ -0,0 +1,13 @@
+package com.xiaowu5759.common.web;
+
+/**
+ * web返回前端处理
+ * 1. 时间yyyy-MM-dd HH:mm:ss
+ * 2. long型传给前端变成string
+ * 3. BigDecimal，在业务中处理，返回前端需要的，转换成string
+ *
+ * @author xiaowu5759
+ * @since 2021/8/8 13:31
+ */
+public class WebConfig {
+}
